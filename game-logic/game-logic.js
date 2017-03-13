c40544d3b1546cdc22040572049fb9050901b7e9
Sean McDermott <mcderm.sean@gmail.com>
2017-03-13 14:31:09 -0700

    able to hard code in computer pattern

--- game-logic/game-logic.js
+++ game-logic/game-logic.js
@@ -1,25 +1,19 @@
 console.log('game-logic sanity check!');
+let boxPattern = [1,2,3,4,5];
 
-$('.game-gui').on('mousedown', '.col', function (){
-  $(this).toggleClass('accent-1')
-})
-$('.game-gui').on('mouseup', '.col', function (){
-  $(this).toggleClass('accent-1')
-})
+function boxToggle(id, delay) {
+  $(`#${id}`).delay(0).animate(
+    { opacity: '1' }, 0, 'swing', function () { $(this).addClass('accent-1');
+   });
+  $(`#${id}`).delay(delay).animate(
+    { opacity: '1' }, 0, 'swing', function () { $(this).removeClass('accent-1');
+  });
+}
 
-$('body').on('keydown keyup', function (e){
-  let keyAction = {
-    87 : '#1',
-    69 : '#2',
-    65 : '#3',
-    83 : '#5',
-    68 : '#4',
-  };
-  let key = e.which;
-  let keyID = keyAction[key];
-  let boxID;
-  if (typeof keyID != 'undefined'){
-    boxID = keyID;
-    $(boxID).toggleClass('accent-1');
-  }
+$('#start-btn').on('click', function() {
+   boxToggle(1,500);
+   boxToggle(2,500);
+   boxToggle(3,500);
+   boxToggle(4,500);
+   boxToggle(5,500);
 })
