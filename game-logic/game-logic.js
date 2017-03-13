33121ff7e1cc3ef8f0d46c552853e3a8d8687381
Sean McDermott <mcderm.sean@gmail.com>
2017-03-13 14:37:00 -0700

    tweaked hardcoded transition times

--- game-logic/game-logic.js
+++ game-logic/game-logic.js
@@ -1,19 +1,19 @@
 console.log('game-logic sanity check!');
 let boxPattern = [1,2,3,4,5];
 
-function boxToggle(id, delay) {
-  $(`#${id}`).delay(0).animate(
+function boxToggle(id, dStart) {
+  $(`#${id}`).delay(dStart).animate(
     { opacity: '1' }, 0, 'swing', function () { $(this).addClass('accent-1');
    });
-  $(`#${id}`).delay(delay).animate(
-    { opacity: '1' }, 0, 'swing', function () { $(this).removeClass('accent-1');
+  $(`#${id}`).delay(600).animate(
+    { opacity: '1' }, 100, 'swing', function () { $(this).removeClass('accent-1');
   });
 }
 
 $('#start-btn').on('click', function() {
-   boxToggle(1,500);
-   boxToggle(2,500);
-   boxToggle(3,500);
-   boxToggle(4,500);
-   boxToggle(5,500);
+   boxToggle(1,0);
+   boxToggle(2,800);
+   boxToggle(3,1600);
+   boxToggle(4,2400);
+   boxToggle(5,3200);
 })
