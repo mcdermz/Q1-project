53fb665702cd5861bc52c71cabc97fd8667f70b7
Sean McDermott <mcderm.sean@gmail.com>
2017-03-13 11:32:44 -0700

    able to click on boxes to activate accent colors

--- game-logic/game-logic.js
+++ game-logic/game-logic.js
@@ -1,1 +1,8 @@
 console.log('game-logic sanity check!');
+
+$('.game-gui').on('mousedown', '.col', function (){
+  $(this).toggleClass('accent-1')
+})
+$('.game-gui').on('mouseup', '.col', function (){
+  $(this).toggleClass('accent-1')
+})
