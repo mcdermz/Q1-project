9629997a5c9e02b2d60115782143292b1ea168ac
Sean McDermott <mcderm.sean@gmail.com>
2017-03-13 16:35:32 -0700

    cleaned up dead code, GUI now disabled during automated pattern

--- game-logic/game-logic.js
+++ game-logic/game-logic.js
@@ -1,18 +1,12 @@
 console.log('game-logic sanity check!');
-let boxPattern = [1,2,3,4,5,4,3,2,1];
 
-function boxToggle(id, dStart) {
-  $(`#${id}`).delay(dStart).animate(
-    { opacity: '1' }, 0, 'linear', function () { $(this).addClass('accent-1');
-   });
-  $(`#${id}`).delay(600).animate(
-    { opacity: '1' }, 100, 'linear', function () { $(this).removeClass('accent-1');
-  });
-}
+let boxPattern = [1,4,2,3];
 
-let i
+let i;
+let uI = true;
 
 function startGame(i){
+  uI = false;
   let boxActive = `#${boxPattern[i]}`;
   $.when($(boxActive)
     .animate({opacity: '1'}, 300, 'linear', function() {
@@ -25,6 +19,9 @@ function startGame(i){
     i++;
     if (i<boxPattern.length){
       startGame(i);
+    } else{
+      alert('pattern is done, your turn!')
+      uI = true;
     }
   });
 }
@@ -32,10 +29,4 @@ function startGame(i){
 $('#start-btn').on('click', function() {
     i = 0;
     startGame(i);
-  })
-  //  boxToggle(1,0);
-  //  boxToggle(2,800);
-  //  boxToggle(3,1600);
-  //  boxToggle(4,2400);
-  //  boxToggle(5,3200);
-  //  boxToggle(1,4000);
+  });
--- gui/gui.js
+++ gui/gui.js
@@ -1,24 +1,26 @@
 console.log('gui sanity check!');
 
-$('.game-gui').on('mousedown', '.col', function (){
-  $(this).toggleClass('accent-1')
+$('.game-gui').on('mousedown', '.col', function () {
+  if (uI)
+    $(this).toggleClass('accent-1')
 })
-$('.game-gui').on('mouseup', '.col', function (){
-  $(this).toggleClass('accent-1')
+$('.game-gui').on('mouseup', '.col', function () {
+  if (uI)
+    $(this).toggleClass('accent-1')
 })
 
-$('body').on('keydown keyup', function (e){
+$('body').on('keydown keyup', function (e) {
   let keyAction = {
-    87 : '#1',
-    69 : '#2',
-    65 : '#3',
-    83 : '#5',
-    68 : '#4',
+    87: '#1',
+    69: '#2',
+    65: '#3',
+    83: '#5',
+    68: '#4',
   };
   let key = e.which;
   let keyID = keyAction[key];
   let boxID;
-  if (typeof keyID != 'undefined'){
+  if (typeof keyID != 'undefined' && uI) {
     boxID = keyID;
     $(boxID).toggleClass('accent-1');
   }
