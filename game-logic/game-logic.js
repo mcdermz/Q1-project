a069c6db871a7e298735dece0ec1d674b3ca99c4
Sean McDermott <mcderm.sean@gmail.com>
2017-03-13 12:13:25 -0700

    keypress now toggles boxes

--- game-logic/game-logic.js
+++ game-logic/game-logic.js
@@ -6,3 +6,20 @@ $('.game-gui').on('mousedown', '.col', function (){
 $('.game-gui').on('mouseup', '.col', function (){
   $(this).toggleClass('accent-1')
 })
+
+$('body').on('keydown keyup', function (e){
+  let keyAction = {
+    87 : '#1',
+    69 : '#2',
+    65 : '#3',
+    83 : '#5',
+    68 : '#4',
+  };
+  let key = e.which;
+  let keyID = keyAction[key];
+  let boxID;
+  if (typeof keyID != 'undefined'){
+    boxID = keyID;
+    $(boxID).toggleClass('accent-1');
+  }
+})
